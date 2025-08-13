9586d6493fd2cc95ad86ac23e80657a1b944e092
Jean-Christian Denis <contact@jcdenis.fr>
2025-08-14 00:55:19 +0200

    release 0.1

---
+++ CHANGELOG.md
@@ -0,0 +1,6 @@
+Oauth2Connect 0.1 - 2025.08.13
+==========================================================
+* Require Dotclear 2.36
+* Require Plugin FrontendSession 0.34
+* Require PHP 8.1
+* First lines of code
---
+++ _define.php
@@ -0,0 +1,34 @@
+<?php
+
+/**
+ * @file
+ * @brief       The plugin Oauth2Connect definition
+ * @ingroup     Oauth2Connect
+ *
+ * @defgroup    Oauth2Connect Plugin cinecturlink2.
+ *
+ * Allow third party connection on frontend.
+ *
+ * @author      Jean-Christian Paul Denis
+ * @copyright   AGPL-3.0
+ */
+declare(strict_types=1);
+
+$this->registerModule(
+    'Oauth2 Connect',
+    'Allow third party connection on frontend.',
+    'Jean-Christian Paul Denis and Contributors',
+    '0.1',
+    [
+        'requires'    => [
+            ['core', '2.36'],
+            ['FrontendSession', '0.34'],
+        ],
+        'permissions' => 'My',
+        'type'        => 'plugin',
+        'support'     => 'https://github.com/JcDenis/' . $this->id . '/issues',
+        'details'     => 'https://github.com/JcDenis/' . $this->id . '/',
+        'repository'  => 'https://raw.githubusercontent.com/JcDenis/' . $this->id . '/master/dcstore.xml',
+        'date'        => '2025-06-25T22:40:59+00:00',
+    ]
+);
